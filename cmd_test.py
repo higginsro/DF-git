62f29196a6bfe5b4baed7eca7f242851b98cffe8
Rory HIGGINS <Rory.HIGGINS@amadeus.com>
2018-05-24 11:35:28 +0100

    deleted scratch python files

--- cmd_test.py
+++
@@ -1,5 +0,0 @@
-import os
-
-
-os.system('git status {}'.format(os.getcwd()))
-# os.system('git config user.name')
--- dulwich_df.py
+++
@@ -1,7 +0,0 @@
-import os
-from dulwich.repo import Repo
-
-repo = Repo(os.getcwd())
-index = repo.open_index()
-print(list(index))
-print(repo, type(repo))
